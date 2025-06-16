603b5fed498fe96c3c3368d0abd198097135b1b7
Mostafa Mahmoud Fouad <mm1471800@gmail.com>
2025-06-16 17:30:31 +0300

    Create fenwic-tree.cpp

---
+++ DS/fenwic-tree.cpp
@@ -0,0 +1,25 @@
+class BIT {
+public:
+    vector<int> bit;
+    int size;
+
+    BIT(int n) : size(n + 1) {
+        bit = vector<int>(size);
+    }
+
+    void add(int idx, int v) {
+        for (++idx; idx < size; idx += idx & -idx)
+            bit[idx] += v;
+    }
+
+    int sum(int idx) {
+        int res{};
+        for (++idx; idx > 0; idx -= idx & -idx)
+            res += bit[idx];
+        return res;
+    }
+
+    int sum(int l, int r) {
+        return sum(r) - sum(l - 1);
+    }
+};
