700a460d6618edee3bf5bb2a8e46923d371442eb
Mostafa Mahmoud Fouad <mm1471800@gmail.com>
2025-07-01 14:05:34 +0300

    Update suffix_array.cpp

--- strings/suffix_array.cpp
+++ strings/suffix_array.cpp
@@ -1,57 +1,42 @@
-vector<int> sort_cyclic_shifts(string const& s)
-{
-    int n = s.size();
-    const int alphabet = 256;
-    vector<int> p(n), c(n), cnt(max(alphabet, n), 0);
-    for (int i = 0; i < n; i++)
-        cnt[s[i]]++;
-    for (int i = 1; i < alphabet; i++)
-        cnt[i] += cnt[i - 1];
-    for (int i = 0; i < n; i++)
-        p[--cnt[s[i]]] = i;
-    c[p[0]] = 0;
-    int classes = 1;
-    for (int i = 1; i < n; i++)
-    {
-        if (s[p[i]] != s[p[i - 1]])
-            classes++;
-        c[p[i]] = classes - 1;
-    }
-    vector<int> pn(n), cn(n);
-    for (int h = 0; (1 << h) < n; ++h)
-    {
-        for (int i = 0; i < n; i++)
-        {
-            pn[i] = p[i] - (1 << h);
-            if (pn[i] < 0)
-                pn[i] += n;
-        }
-        fill(cnt.begin(), cnt.begin() + classes, 0);
-        for (int i = 0; i < n; i++)
-            cnt[c[pn[i]]]++;
-        for (int i = 1; i < classes; i++)
-            cnt[i] += cnt[i - 1];
-        for (int i = n - 1; i >= 0; i--)
-            p[--cnt[c[pn[i]]]] = pn[i];
-        cn[p[0]] = 0;
-        classes = 1;
-        for (int i = 1; i < n; i++)
-        {
-            pair<int, int> cur = {c[p[i]], c[(p[i] + (1 << h)) % n]};
-            pair<int, int> prev = {c[p[i - 1]], c[(p[i - 1] + (1 << h)) % n]};
-            if (cur != prev)
-                ++classes;
-            cn[p[i]] = classes - 1;
-        }
-        c.swap(cn);
-    }
-    return p;
-}
-
+/// O(n log^2(n))
 vector<int> build(string s)
 {
-    s += "$";
-    vector<int> sorted_shifts = sort_cyclic_shifts(s);
-    sorted_shifts.erase(sorted_shifts.begin());
-    return sorted_shifts;
+    s += '$';
+    int n = s.size();
+    vector<int> p(n), c(n);
+    {
+        /// k=0
+        vector<pair<int,int>> a(n);
+        for (int i = 0; i < n; i++)a[i] = {s[i], i};
+        sort(a.begin(), a.end());
+        for (int i = 0; i < n; i++)p[i] = a[i].S;
+        c[p[0]] = 0;
+        for (int i = 1; i < n; i++)
+        {
+            if (a[i].F == a[i - 1].F)
+                c[p[i]] = c[p[i - 1]];
+            else
+                c[p[i]] = c[p[i - 1]] + 1;
+        }
+    }
+    int k = 0;
+    while ((1 << k) < n)
+    {
+        /// calculate k+1
+        vector<pair<pair<int,int>,int>> a(n);
+        for (int i = 0; i < n; i++)
+            a[i] = {{c[i], c[(i + (1 << k)) % n]}, i};
+        sort(a.begin(), a.end());
+        for (int i = 0; i < n; i++)p[i] = a[i].S;
+        c[p[0]] = 0;
+        for (int i = 1; i < n; i++)
+        {
+            if (a[i].F == a[i - 1].F)
+                c[p[i]] = c[p[i - 1]];
+            else
+                c[p[i]] = c[p[i - 1]] + 1;
+        }
+        k++;
+    }
+    return p;
 }
