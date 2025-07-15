3a39edaf94ef620142dccff987662cd988e61c78
AhmedM126 <ahmedmansour1044@gmail.com>
2025-07-15 22:41:27 +0300

    Update XOR Basis with bitset.cpp

--- math&number-theory/XOR Basis with bitset.cpp
+++ math&number-theory/XOR Basis with bitset.cpp
@@ -1,19 +1,19 @@
-const int B = 190;
+const int B = 251;
 struct Basis {
     int sz = 0;
-    vector<bitset<170>> b;
- 
-    Basis() : b(vector<bitset<170>>(B)) {}
- 
-    bool all_zeros(bitset<170> &x) {
+    vector<bitset<B>> b;
+
+    Basis() : b(vector<bitset<B>>(B)) {}
+
+    bool all_zeros(bitset<B> &x) {
         bool f = 1;
         for (int i = 0; i < x.size(); i++)
             if (x[i])f = 0;
- 
+
         return f;
     }
- 
-    void insert(bitset<170> x) {
+
+    void insert(bitset<B> x) {
         for (int i = B - 1; ~i; --i) {
             if (!x[i]) continue;
             if (all_zeros(b[i])) {
@@ -23,8 +23,8 @@ struct Basis {
             x ^= b[i];
         }
     }
- 
-    bool can(bitset<170> x) {
+
+    bool can(bitset<B> x) {
         for (int i = B - 1; ~i; --i) {
             if (!x[i]) continue;
             if (all_zeros(b[i])) return 0;
