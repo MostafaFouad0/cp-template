c920aa84c2c7553a4398d77b69c2c69fa3ffa095
AhmedM126 <ahmedmansour1044@gmail.com>
2025-07-15 18:48:22 +0300

    Update XOR Basis.cpp

--- math&number-theory/XOR Basis.cpp
+++ math&number-theory/XOR Basis.cpp
@@ -1,4 +1,4 @@
-// how many ways to construct x using xors = 2^(n-k), n is size of th vector space and k is the size of the basis
+// how many ways to construct x using xors = 2^(n-k), n is size of the vector space and k is the size of the basis
 /* you have to at most iterate over the size of the size of the basis to check if there is an xor that equals zero 
     ex if B = 30 then you have to iterate at most 30
 */
