bc7c3c1143a89edfaed8aec427ec62c889db48bc
Mostafa Mahmoud Fouad <mm1471800@gmail.com>
2025-07-28 20:08:14 +0300

    Create template.cpp

---
+++ miscellaneous/template.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+#define int ll
+const int N = 1e5;
+#define deb(x) cout<<#x<<"="<<x<<endl;
+#define F first
+#define S second
+
+void solve()
+{
+    
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cout.tie(NULL);
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("output.txt", "w", stdout);
+    freopen("input.txt", "r", stdin);
+#endif
+    int tt = 1;
+    cin >> tt;
+    for (int i = 0; i < tt; i++)
+    {
+        solve();
+        cout << "\n";
+    }
+}
