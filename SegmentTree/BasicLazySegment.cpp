71b869c1e9ba143da6ae3d60a3275e88df41faca
Salma karam <116811275+Whatismynamee@users.noreply.github.com>
2025-08-18 21:58:45 +0300

    Create BasicLazySegment.cpp

---
+++ SegmentTree/BasicLazySegment.cpp
@@ -0,0 +1,50 @@
+struct Node {
+    //
+};
+
+struct sg {
+    vector<Node> seg;
+    int sz;
+
+    sg(int size) {
+        sz = 1;
+        while (sz < size) sz <<= 1;
+        seg = vector<Node>(sz << 1);
+    }
+
+    void apply(int x, int lx, int rx, int v) {
+        //apply operation
+    }
+
+    void push(int x, int lx, int rx) {
+        if (seg[x].lazy && lx != rx) {
+            int md = (lx + rx) >> 1;
+            apply(2 * x + 1, lx, md, seg[x].lazy);
+            apply(2 * x + 2, md + 1, rx, seg[x].lazy);
+        }
+        //return the lazy to -1
+    }
+
+    void update(int l, int r, int v, int x = 0, int lx = 0, int rx = -1) {
+        if (!~rx) rx = sz - 1;
+        push(x, lx, rx);
+        if (r < lx or rx < l) return;
+        if (l <= lx and rx <= r) {
+            apply(x, lx, rx, v);
+            push(x, lx, rx);
+            return;
+        }
+        int md = lx + rx >> 1;
+        update(l, r, v, 2 * x + 1, lx, md);
+        update(l, r, v, 2 * x + 2, md + 1, rx);
+    }
+
+    int query(int l, int r, int x = 0, int lx = 0, int rx = -1) {
+        if (!~rx) rx = sz - 1;
+        push(x, lx, rx);
+        if (r < lx or rx < l) return //;
+        if (l <= lx and rx <= r) return //;
+        int md = lx + rx >> 1;
+        return //(query(l, r, 2 * x + 1, lx, md), query(l, r, 2 * x + 2, md + 1, rx));
+    }
+};
