5b0615ce874d37dbba9cb36ab31630ce39853828
Salma karam <116811275+Whatismynamee@users.noreply.github.com>
2025-08-31 22:56:36 +0300

    Update DP(SOS).cpp

--- DP/DP(SOS).cpp
+++ DP/DP(SOS).cpp
@@ -1,20 +1,24 @@
-//number of elements that x&y == x
+//number of elements that x&y == x  (1)
 for (int i = 0; i < 20; ++i) {
         for (int msk = N - 1; msk >= 0; --msk) {
             if (!(msk & (1 << i))) dp2[msk] += dp2[msk ^ (1 << i)];
         }
     }
 
-//number of elements that x|y == x
+//number of elements that x|y == x  (2)
 for (int i = 0; i < 20; ++i) {
         for (int msk = 0; msk < N; ++msk) {
             if (msk & (1 << i)) dp1[msk] += dp1[msk ^ (1 << i)];
         }
     }
 
-//subsequence there OR exactly == x
+//subsequence there OR exactly == x  (3)
 for (int i = 0; i < n; ++i) {
         for (int msk = 0; msk < (1 << n); ++msk) {
             if (msk & (1 << i)) dp[msk] = (dp[msk] + M - dp[msk ^ (1 << i)]) % M;
         }
     }
+
+//number of elements that x&y != 0 
+//use method 2 and answer is -> (total - dp[msk^((1<<n)-1)])
+
