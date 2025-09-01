5fe104db8cd67a43f2880f1f612a145a5649c08c
Salma karam <116811275+Whatismynamee@users.noreply.github.com>
2025-09-01 07:15:26 +0300

    Update DP(SOS).cpp

--- DP/DP(SOS).cpp
+++ DP/DP(SOS).cpp
@@ -28,5 +28,16 @@ for(int i = mask; i > 0; i = (i-1) & mask){
     	//gurantee that (mask|i==mask)
 }
 
+//if i'm looping on the masks and i need the answer of a previous mask
+for(int mask = 0; mask < (1<<N); ++mask){
+	dp[mask][-1] = A[mask];	//handle base case separately (leaf states)
+	for(int i = 0;i < N; ++i){
+		if(mask & (1<<i))
+			dp[mask][i] = dp[mask][i-1] + dp[mask^(1<<i)][i-1];
+		else
+			dp[mask][i] = dp[mask][i-1];
+	}
+	F[mask] = dp[mask][N-1];
+}
 
 
