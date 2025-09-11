af16ac1f3e92d3af24f2630315ffce972e1fead4
Salma karam <116811275+Whatismynamee@users.noreply.github.com>
2025-09-12 00:52:07 +0300

    Update FWHTwithoutMod.cpp

--- fft/FWHTwithoutMod.cpp
+++ fft/FWHTwithoutMod.cpp
@@ -21,24 +21,6 @@ void ifwht_or(vector<int> &a) {
     }
 }
 
-vector<int> conv_or(vector<int> &a, vector<int> &b) {
-    int n = max(a.size(), b.size());
-    int sz = 1;
-    while (sz < n) sz <<= 1;
-
-    vector<int> A = a, B = b;
-    A.resize(sz);
-    B.resize(sz);
-
-    fwht_or(A);
-    fwht_or(B);
-
-    for (int i = 0; i < sz; ++i) A[i] *= B[i];
-
-    ifwht_or(A);
-    return A;
-}
-
 //AND
 void fwht_and(vector<int> &a) {
     int n = a.size();
@@ -62,6 +44,33 @@ void ifwht_and(vector<int> &a) {
     }
 }
 
+//XOR
+void fwht_xor(vector<int> &a) {
+    int n = a.size();
+    for (int s = 2, h = 1; s <= n; s <<= 1, h <<= 1) {
+        for (int l = 0; l < n; l += s) {
+            for (int i = 0; i < h; ++i) {
+                int tmp = a[l + h + i];
+                a[l + h + i] = a[l + i] - tmp;
+                a[l + i] += tmp;
+            }
+        }
+    }
+}
+
+void ifwht_xor(vector<int> &a) {
+    int n = a.size();
+    for (int s = 2, h = 1; s <= n; s <<= 1, h <<= 1) {
+        for (int l = 0; l < n; l += s) {
+            for (int i = 0; i < min(i, n - l - h); ++i) {
+                int tmp = a[l + h + i];
+                a[l + h + i] = (a[l + i] - tmp) / 2;
+                a[l + i] = (a[l + i] + tmp) / 2;
+            }
+        }
+    }
+}
+
 vector<int> conv_and(vector<int> &a, vector<int> &b) {
     int n = max(a.size(), b.size());
     int sz = 1;
@@ -71,19 +80,14 @@ vector<int> conv_and(vector<int> &a, vector<int> &b) {
     A.resize(sz);
     B.resize(sz);
 
-    fwht_and(A);
-    fwht_and(B);
+    fwht_xor(A);//or fwht_or or fwht_and
+    fwht_xor(B);//or fwht_or or fwht_and
 
     for (int i = 0; i < sz; ++i) A[i] *= B[i];
 
-    ifwht_and(A);
+    ifwht_xor(A);//or ifwht_or or ifwht_and
     return A;
 }
-
-//XOR
-
-
-
 
 vector<int> poly_pow(vector<int> &a, int p) {
     vector<int> res;
