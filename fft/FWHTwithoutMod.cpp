0803739d1c1dff699c39ada9830ea8274af644aa
Salma karam <116811275+Whatismynamee@users.noreply.github.com>
2025-09-12 00:06:17 +0300

    Create FWHTwithoutMod.cpp

---
+++ fft/FWHTwithoutMod.cpp
@@ -0,0 +1,41 @@
+//OR
+void fwht_or(vector<int> &a) {
+    int n = a.size();
+    for (int s = 2, h = 1; s <= n; s <<= 1, h <<= 1) {
+        for (int l = 0; l < n; l += s) {
+            for (int i = 0; i < h; ++i) {
+                a[l + h + i] += a[l + i];
+            }
+        }
+    }
+}
+
+void ifwht_or(vector<int> &a) {
+    int n = a.size();
+    for (int s = 2, h = 1; s <= n; s <<= 1, h <<= 1) {
+        for (int l = 0; l < n; l += s) {
+            for (int i = 0; i < h; ++i) {
+                a[l + h + i] -= a[l + i];
+            }
+        }
+    }
+}
+
+vector<int> conv_or(vector<int> &a, vector<int> &b) {
+    int n = max(a.size(), b.size());
+    int sz = 1;
+    while (sz < n) sz <<= 1;
+
+    vector<int> A = a, B = b;
+    A.resize(sz);
+    B.resize(sz);
+
+    fwht_or(A);
+    fwht_or(B);
+
+    for (int i = 0; i < sz; ++i) A[i] *= B[i];
+
+    ifwht_or(A);
+    return A;
+}
+
