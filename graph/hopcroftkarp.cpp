36ee49a0bdf1c4dd0fc3587465e1fa9d5aeeeb33
Salma karam <116811275+Whatismynamee@users.noreply.github.com>
2025-09-13 06:10:57 +0300

    Update hopcroftkarp.cpp

--- graph/hopcroftkarp.cpp
+++ graph/hopcroftkarp.cpp
@@ -1,88 +1,61 @@
 // O( sqrt(n) * m )
 struct HopcroftKarp {
- private:
-  vector<vector<int>> *g = nullptr;
-  int n, m;
-  vector<int> dis;
-  void bfs() {
-    queue<int> q;
-    for (int i = 0; i < n; ++i) {
-      if (matchL[i] == -1) {
-        dis[i] = 0;
-        q.push(i);
-      } else {
-        dis[i] = -1;
-      }
-    }
-    while (!q.empty()) {
-      int u = q.front();
-      q.pop();
-      for (int v : (*g)[u]) {
-        if (matchR[v] != -1 && dis[matchR[v]] == -1) {
-          dis[matchR[v]] = dis[u] + 1;
-          q.push(matchR[v]);
-        }
-      }
-    }
-  };
-  bool dfs(int u) {
-    for (int v : (*g)[u]) {
-      if (matchR[v] == -1 || (dis[matchR[v]] == dis[u] + 1 && dfs(matchR[v]))) {
-        matchL[u] = v;
-        matchR[v] = u;
-        return true;
-      }
-    }
-    // dis[u] = -1;
-    return false;
-  };
+    static const int inf = 1e9;
+    int n;
+    vector<int> l, r, d;
+    vector<vector<int>> g;
 
- public:
-  vector<int> matchL, matchR;
-  void SetGraph(int numleft, int numright, vector<vector<int>> *graph) {
-    n = numleft;
-    m = numright;
-    g = graph;
-    assert(g != nullptr && g->size() == n);
-    matchL.resize(n);
-    matchR.resize(m);
-    dis.resize(n);
-  }
-  int max_match() {
-    assert(g != nullptr);
-    fill(matchL.begin(), matchL.end(), -1);
-    fill(matchR.begin(), matchR.end(), -1);
-    int ans = 0;
-    while (true) {
-      bfs();
-      int add = 0;
-      for (int i = 0; i < n; ++i) {
-        if (matchL[i] == -1 && dfs(i)) {
-          add++;
+    HopcroftKarp(int _n, int _m) {
+        n = _n;
+        int p = _n + _m + 1;
+        g.resize(p);
+        l.resize(p, 0);
+        r.resize(p, 0);
+        d.resize(p, 0);
+    }
+
+    void add_edge(int u, int v) {
+        g[u].push_back(v + n); //right id is increased by n, so is l[u]
+    }
+
+    bool bfs() {
+        queue<int> q;
+        for (int u = 1; u <= n; u++) {
+            if (!l[u]) d[u] = 0, q.push(u);
+            else d[u] = inf;
         }
-      }
-      if (add == 0) break;
-      ans += add;
+        d[0] = inf;
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            for (auto v: g[u]) {
+                if (d[r[v]] == inf) {
+                    d[r[v]] = d[u] + 1;
+                    q.push(r[v]);
+                }
+            }
+        }
+        return d[0] != inf;
     }
-    return ans;
-  }
+
+    bool dfs(int u) {
+        if (!u) return true;
+        for (auto v: g[u]) {
+            if (d[r[v]] == d[u] + 1 && dfs(r[v])) {
+                l[u] = v;
+                r[v] = u;
+                return true;
+            }
+        }
+        d[u] = inf;
+        return false;
+    }
+
+    int maximum_matching() {
+        int ans = 0;
+        while (bfs()) {
+            for (int u = 1; u <= n; u++) if (!l[u] && dfs(u)) ans++;
+        }
+        return ans;
+    }
 };
-int main() {
-  int n, m, e;
-  cin >> n >> m >> e;
-  vector<vector<int>> g(n);
-  for (int i = 0; i < e; ++i) {
-    int a, b;
-    cin >> a >> b;
-    g[a].push_back(b);
-  }
-  HopcroftKarp hk;
-  hk.SetGraph(n, m, &g);
-  cout << hk.max_match() << '\n';
-  for (int i = 0; i < n; ++i) {
-    if (hk.matchL[i] != -1) {
-      cout << i << ' ' << hk.matchL[i] << '\n';
-    }
-  }
-  return 0;
-}
