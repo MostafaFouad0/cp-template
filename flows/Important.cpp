ad0849ac3cb190e209f40ab8df82893bcead2caf
Salma karam <116811275+Whatismynamee@users.noreply.github.com>
2025-09-14 06:58:21 +0300

    Update Important.cpp

--- flows/Important.cpp
+++ flows/Important.cpp
@@ -1,1 +1,12 @@
-
+// Common matching variations
+// --------------------------
+//              |               Bipartite               |   Non-Bipartite
+// +------------+---------------------------------------+------------------+
+// | Unweighted | - Max flow algorithms                 | - Edmond's       |
+// |   Edges    | - Repeated augmenting paths with DFS  |   blossom alg.   |
+// |            | - Hopcroft-Karp                       |                  |
+// +------------+---------------------------------------+------------------+
+// | Weighted   | - Min cost max flow algorithms        | - DP solution    |
+// |   Edges    | - Hungarian algorithm (perfect match) |   for small      |
+// |            | - LP network simplex                  |   graphs         |
+// +------------+---------------------------------------+------------------+
