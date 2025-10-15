14392358ea63a3ba77c8efad7ac160b29b41ca4c
Salma karam <116811275+Whatismynamee@users.noreply.github.com>
2025-10-15 08:13:21 +0300

    Create Centroid_Decomposition.cpp

---
+++ Tree/Centroid_Decomposition.cpp
@@ -0,0 +1,32 @@
+vector<vector<int>> adj;
+vector<int> sub;
+vector<bool> removed;
+
+int get_size(int u, int v = -1) {
+    sub[u] = 1;
+    for (auto i: adj[u]) {
+        if (i == v) continue;
+        sub[u] += get_size(i, u);
+    }
+    return sub[u];
+}
+
+int get_centroid(int u, int sz, int v = -1) {
+    for (auto i: adj[u]) {
+        if (i == v or removed[i]) continue;
+        if (sub[i] * 2 > sz) get_centroid(i, sz, u);
+    }
+    return u;
+}
+
+void build_centroid(int u) {
+    int centroid = get_centroid(u, get_size(u));
+    //
+    // the function will be here..
+    //
+    removed[centroid] = true;
+    for (auto i: adj[centroid]) {
+        if (removed[i]) continue;
+        build_centroid(i);
+    }
+}
