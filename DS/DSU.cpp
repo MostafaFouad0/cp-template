4a2c7de5d6d9c370e3c194c9b74549b284f790bd
Salma karam <116811275+Whatismynamee@users.noreply.github.com>
2025-11-12 17:15:41 +0200

    Update DSU.cpp

--- DS/DSU.cpp
+++ DS/DSU.cpp
@@ -1,50 +1,42 @@
 struct DSU {
-    vector<int> par, sz, roll;
-    vector<pair<int, int>> update;
-    int comp;
- 
-    DSU() {}
- 
-    DSU(int size) {// 1-based
-        comp = size;
-        par = sz = vector<int>(size + 1);
+    vector<int> parent, GroupSize;
+
+    DSU(int size) {
+        parent = GroupSize = vector<int>(size + 1);
         for (int i = 1; i <= size; ++i) {
-            par[i] = i;
-            sz[i] = 1;
+            parent[i] = i;
+            GroupSize[i] = 1;
         }
     }
- 
-    int Findleader(int u) {
-        if (par[u] == u) return u;
-        return Findleader(par[u]);
+
+    int FindLeader(int node) {
+        if (parent[node] == node)return node;
+        return parent[node] = FindLeader(parent[node]);
     }
- 
-    bool merge(int u, int v) {
-        u = Findleader(u);
-        v = Findleader(v);
-        if (u == v) return false;
-        comp--;
-        if (sz[u] < sz[v]) swap(u, v);
-        update.emplace_back(v, u);
-        sz[u] += sz[v];
-        par[v] = u;
-        return true;
+
+    bool SameGroup(int u, int v) {
+        int leader1 = FindLeader(u);
+        int leader2 = FindLeader(v);
+        return leader1 == leader2;
     }
- 
-    void snap() {
-        roll.emplace_back(update.size());
+
+    int GetSize(int node) {
+        int leader = FindLeader(node);
+        return GroupSize[leader];
     }
- 
-    void undo() {
-        if (roll.empty()) return;
-        while (roll.back() < (int) update.size()) {
-            int u = update.back().first, v = update.back().second;
-            update.pop_back();
-            sz[v] -= sz[u];
-            par[u] = u;
-            comp++;
+
+    void MergeGroups(int u, int v) {
+        int leader1 = FindLeader(u);
+        int leader2 = FindLeader(v);
+        if (leader1 == leader2)return;
+        if (GroupSize[leader1] > GroupSize[leader2]) {
+            parent[leader2] = leader1;
+            GroupSize[leader1] += GroupSize[leader2];
+            return;
         }
-        roll.pop_back();
+        parent[leader1] = leader2;
+        GroupSize[leader2] += GroupSize[leader1];
+
     }
- 
+
 };
