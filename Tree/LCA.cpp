a5eeb90627c716982db1c4f4d621c0295bd65a69
Salma karam <116811275+Whatismynamee@users.noreply.github.com>
2025-11-23 07:17:30 +0200

    Update LCA.cpp

--- Tree/LCA.cpp
+++ Tree/LCA.cpp
@@ -1,9 +1,6 @@
-void dfs(int u, int par)
-{
-    for (auto it : adj[u])
-    {
-        if (par != it)
-        {
+void dfs(int u, int par) {
+    for (auto it: adj[u]) {
+        if (par != it) {
             lvl[it] = lvl[u] + 1;
             dp[0][it] = u;
             dfs(it, u);
@@ -11,37 +8,29 @@ void dfs(int u, int par)
     }
 }
 
-void build()
-{
-	// build LCA
-    for (int mask = 1; mask < 25; mask++)
-    {
-        for (int u = 1; u <= n; ++u)
-        {
+void build() {
+    // build LCA
+    for (int mask = 1; mask < LOG; mask++) {
+        for (int u = 1; u <= n; ++u) {
             if (dp[mask - 1][u] != -1)
                 dp[mask][u] = dp[mask - 1][dp[mask - 1][u]];
         }
     }
 }
 
-int LCA(int u, int v)
-{
+int LCA(int u, int v) {
     if (lvl[u] < lvl[v])swap(u, v);
     int diff = lvl[u] - lvl[v];
-    for (int i = 0; diff; i++, diff >>= 1)
-    {
-        if (diff & 1)
-        {
+    for (int i = 0; diff; i++, diff >>= 1) {
+        if (diff & 1) {
             u = dp[i][u];
         }
     }
     if (u == v)
         return u;
-    for (int i = 21; i >= 0; i--)
-    {
+    for (int i = LOG - 1; i >= 0; i--) {
         int pu = dp[i][u], pv = dp[i][v];
-        if (pu != -1 and pv != -1 and pu != pv)
-        {
+        if (pu != -1 and pv != -1 and pu != pv) {
             u = pu, v = pv;
         }
     }
